17a70d5e47f1c1b7479a45afd1b1f0f70b5b1a2e
jenjones319 <jenjones605@gmail.com>
2020-06-16 16:09:46 -0500

    added js content

--- js/app.js
+++ js/app.js
@@ -1,46 +1,63 @@
-'use script'
-console.log('This file is connected.')
+'use strict';
+console.log('This file is connected.');
 
 var userName = prompt('What is your name?');
-var lowerCaseName = userName.toLowerCase();
-console.log(lowerCaseName);
+// console.log(userName);
 alert('Hello ' + userName);
 
+var lowerCaseName = userName.toLowerCase();
+/* console.log(lowerCaseName); */
 
-if (lowerCaseName === 'jen' || lowerCaseName === 'hank'); {
-  alert('Welcome back to my page ' + userName);
+if(lowerCaseName === 'jen' || lowerCaseName === 'hank') {
+  alert('Welcome back, ' + userName + ' I missed you!');
 } else {
   alert('Welcome, friend, to my page.');
 }
 
-var myEduc = ('Did I go to college in the United States?');
+var myEduc = prompt('Did I go to college in the United States? Yes or No.');
+// console.log(myEduc);
+
 var upperCaseEduc = myEduc.toUpperCase();
-console.log(upperCaseEduc);
+/* console.log(upperCaseEduc); */
 
-if (myEduc = 'YES'); {
-alert ('Correct!');
-} else {
-  alert ('Oh, snap! It\'s true. I went to USD in South Dakota.');
+if (myEduc === 'Yes') {
+  alert ('Correct!');
+} if (myEduc = 'no') {
+  alert('Oh, snap! It\'s true. I went to USD in South Dakota.');
 }
 
-var myBooleanValueTrue = true
-var anotherBooleanValueFalse = false
-var thirdBooleanValueTrue = true
+var myTravel = prompt('Do I want to travel to China? Yes or No.');
+// console.log(myTravel);
 
-if (myBooleanValueTrue && thirdBooleanValueTrue) {
-  alert('The && will run if both are true')
-  console.log('both values were true')
-} else if (myBooleanValueTrue || anotherBooleanValueFalse) {
-  alert('both values are true so this will run')
-  console.log('both values are true')
-} else if (thirdBooleanValueTrue) {
-  alert('the third value was true so this will run')
-  console.log('thirdBooleanValueTrue was true')
-} else {
-  console.log('There were no true values')
+var lowerCaseTravel = myTravel.toLowerCase();
+/* console.log(lowerCaseTravel); */
+
+if (myTravel = 'no') {
+  alert ('You are right. I don\'t want to go to China.');
+} if (myTravel = 'yes') {
+  alert('China is not on my bucket list.');
 }
 
-var emptyArray = ();
+var myPets = prompt('Do I have any pets? Y or N?');
+// consolelog(myPets);
 
-var quizAnswer = ['b', 'c', 'd', 'a', 'c'];
-var nestedArray = [['yes','y'], 
+var lowerCasePets = myPets.toLowerCase();
+/* console.log(lowerCasePets); */
+
+if (myPets === 'Y') {
+  alert('Yes, I do! Correct.');
+} else {
+  alert('Nope, the answer is Y. I love dogs.');
+}
+
+var myKids = prompt('Do I have any children? Yes or No?');
+// console.log(myKids);
+
+var upperCaseKids = myKids.toUpperCase();
+/* console.log(upperCaseKids); */
+
+if (myKids = 'yes') {
+  alert('You\'re a good guesser!');
+} if (myKids = 'no') {
+  alert('Yeah, I do. I have a son.');
+}
