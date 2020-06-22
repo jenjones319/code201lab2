a888206889159c08f7c26978b4cd89ad1d8f9b1d
jenjones319 <jenjones605@gmail.com>
2020-06-22 00:07:17 -0500

    removed all console log msgs

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
 'use strict';
-console.log('This file is connected.');
 
 var userPoints = 0
 var userName
 
 function getUserName() {
 userName = prompt('What is your name?');
-// console.log(userName);
 alert('Hello ' + userName);
 
 var lowerCaseName = userName.toLowerCase();
-/* console.log(lowerCaseName); */
 
 if (lowerCaseName === 'jen') {
   alert('Welcome back, ' + userName + ' I missed you!');
@@ -23,16 +20,13 @@ getUserName();
 
 function getEducation() {
 var myEduc = prompt('Did I go to college in the United States? Yes or No.');
-// console.log(myEduc);
 
 var upperCaseEduc = myEduc.toUpperCase();
-/* console.log(upperCaseEduc); */
 
 if (upperCaseEduc === 'YES' || upperCaseEduc === 'Y') {
   alert('Correct!');
   userPoints = userPoints + 1;
   userPoints += 1;
-  console.log('This is the user point total', userPoints);
 } else {
   alert('Oh, snap! It\'s true. I went to USD in South Dakota.');
 }
@@ -42,16 +36,13 @@ getEducation ();
 
 function getTravel() {
 var myTravel = prompt('Do I want to travel to China? Yes or No.');
-// console.log(myTravel);
 
 var lowerCaseTravel = myTravel.toLowerCase();
-/* console.log(lowerCaseTravel); */
 
 if (lowerCaseTravel === 'no' || lowerCaseTravel === 'n') {
   alert('You are right. I don\'t want to go to China.');
   userPoints = userPoints + 1;
   userPoints += 1;
-  console.log('This is the user point total', userPoints);
 } else {
   alert('China is not on my bucket list.');
 }
@@ -61,16 +52,13 @@ getTravel();
 
 function getPets() {
 var myPets = prompt('Do I have any pets? Y or N?');
-// consolelog(myPets);
 
 var lowerCasePets = myPets.toLowerCase();
-// console.log(lowerCasePets);
 
 if (lowerCasePets === 'y' || lowerCasePets === 'yes') {
   alert('Yes, I do! I am a dog person. Correct!');
   userPoints = userPoints + 1;
   userPoints += 1;
-  console.log('This is the user point total', userPoints);
 } else {
   alert('Nope, the answer is yes. I love dogs.');
 }
@@ -81,16 +69,13 @@ getPets();
 function getKids() {
 
 var myKids = prompt('Do I have any children? Yes or No?');
-// console.log(myKids);
 
 var upperCaseKids = myKids.toUpperCase();
-/* console.log(upperCaseKids); */
 
 if (upperCaseKids === 'YES' || upperCaseKids === 'Y') {
   alert('You\'re a good guesser!');
   userPoints = userPoints + 1;
   userPoints += 1;
-  console.log('This is the user point total', userPoints);
 } else {
   alert('Yeah, I do. I have a son.');
 }
@@ -98,26 +83,22 @@ if (upperCaseKids === 'YES' || upperCaseKids === 'Y') {
 
 getKids();
 
-//var userPoints = 0;
 
 function getHank() {
 var answer = prompt('Is my dog named Hank?');
-//console.log(answer);
 
 if (answer === 'yes' || answer === 'y') {
   alert('It\'s true, his full name is Hank Hill.');
   userPoints = userPoints + 1;
   userPoints += 1;
-  console.log('This is the user point total', userPoints);
 } else {
   alert(' Oh, sorry, no points for you. His name really is hank');
-  console.log('this is the user point total', userPoints);
+
 }
 }
 
 getHank();
 
-//console.log(numberGuess);
 
 function getNumberGuess() {
 for (var i = 0; i < 4; i++) {
@@ -130,7 +111,6 @@ for (var i = 0; i < 4; i++) {
     alert('Awesome job! The correct answer is 9.');
     userPoints = userPoints + 1;
     userPoints += 1;
-    console.log('This is the user point total', userPoints);
     break;
   } else {
     alert('Guess a number between 1 and 10!');
@@ -140,16 +120,11 @@ for (var i = 0; i < 4; i++) {
 
 getNumberGuess();
 
-
 function getPlaces() {
 var placesILike = ['new york', 'chicago', 'miami', 'minneapolis', 'mexico city', 'portland', 'boston', 'los angeles'];
-// placesILike.push('rome');
-// console.log(placesILike);
 
 var lastChance = 6;
 while (lastChance > 0) {
-
-  console.log('Inside while loop');
 
   alert(userName + ' guess what city I have never visited...');
   var guess = prompt('Options are: New York, Chicago, Miami, Minneapolis, Mexico City, Portland, Boston, Los Angeles, Phoenix, Hong Kong, Beijing');
@@ -161,19 +136,16 @@ while (lastChance > 0) {
       match = true;
       break;
     } else {
-      console.log('incorrect answer', placesILike[i]);
     }
   }
   if (match === false) {
     alert('Good job! I\'ve never been there before!');
     userPoints = userPoints + 1;
     userPoints += 1;
-    console.log('This is the user point total', userPoints);
     break;
   } else {
     alert('Try again.');
   }
-  console.log('array push', placesILike);
   lastChance--;
 }
 }
